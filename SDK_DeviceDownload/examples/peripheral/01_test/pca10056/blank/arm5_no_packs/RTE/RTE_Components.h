0e7f295c275ae4aaced00b2876b4fc63c45f15fb
Kyuubi <khoi.nv202647@sis.hust.edu.vn>
2023-11-09 18:36:34 +0700

    test_bug

---
+++ SDK_DeviceDownload/examples/peripheral/01_test/pca10056/blank/arm5_no_packs/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'blinky_pca10056' 
+ * Target:  'nrf52840_xxaa' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
