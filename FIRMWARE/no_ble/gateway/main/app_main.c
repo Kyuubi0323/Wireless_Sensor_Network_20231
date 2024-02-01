81c473b8a1ac40342a1809fb89ffb5e398d8b40d
Duy Truong Hust <truongnro69@gmail.com>
2024-02-01 11:11:33 +0700

    add Bluetooth Mesh

--- FIRMWARE/no_ble/gateway/main/app_main.c
+++ FIRMWARE/no_ble/gateway/main/app_main.c
@@ -39,6 +39,81 @@
 #include "spiffs/spiffs.h"
 #include "web_server/web_server.h"
 
+#include "esp_ble_mesh_defs.h"
+#include "esp_ble_mesh_common_api.h"
+#include "esp_ble_mesh_networking_api.h"
+#include "esp_ble_mesh_provisioning_api.h"
+#include "esp_ble_mesh_config_model_api.h"
+#include "esp_ble_mesh_lighting_model_api.h"
+#include "esp_ble_mesh_generic_model_api.h"
+#include "ble_mesh_example_init.h"
+
+
+#define MSG_SEND_TTL        3
+#define MSG_SEND_REL        false
+#define MSG_TIMEOUT         0
+#define MSG_ROLE            ROLE_NODE
+
+static uint8_t dev_uuid[16] = { 0xdd, 0xdd };
+
+static struct esp_ble_mesh_key {
+    uint16_t net_idx;
+    uint16_t app_idx;
+    uint8_t  app_key[ESP_BLE_MESH_OCTET16_LEN];
+} prov_key;
+
+// static esp_ble_mesh_client_t config_client;
+static esp_ble_mesh_client_t sensor_client;
+static esp_ble_mesh_client_t sensor_client_1;
+
+static esp_ble_mesh_cfg_srv_t config_server = {
+    .relay = ESP_BLE_MESH_RELAY_DISABLED,
+    .beacon = ESP_BLE_MESH_BEACON_ENABLED,
+#if defined(CONFIG_BLE_MESH_FRIEND)
+    .friend_state = ESP_BLE_MESH_FRIEND_ENABLED,
+#else
+    .friend_state = ESP_BLE_MESH_FRIEND_NOT_SUPPORTED,
+#endif
+#if defined(CONFIG_BLE_MESH_GATT_PROXY_SERVER)
+    .gatt_proxy = ESP_BLE_MESH_GATT_PROXY_ENABLED,
+#else
+    .gatt_proxy = ESP_BLE_MESH_GATT_PROXY_NOT_SUPPORTED,
+#endif
+    .default_ttl = 7,
+    /* 3 transmissions with 20ms interval */
+    .net_transmit = ESP_BLE_MESH_TRANSMIT(2, 20),
+    .relay_retransmit = ESP_BLE_MESH_TRANSMIT(2, 20),
+};
+
+ESP_BLE_MESH_MODEL_PUB_DEFINE(sensor_cli_pub, 20, MSG_ROLE);
+
+static esp_ble_mesh_model_t root_models[] = {
+    ESP_BLE_MESH_MODEL_CFG_SRV(&config_server),
+    // ESP_BLE_MESH_MODEL_CFG_CLI(&config_client),
+    ESP_BLE_MESH_MODEL_LIGHT_LIGHTNESS_CLI(&sensor_cli_pub, &sensor_client),
+};
+
+ESP_BLE_MESH_MODEL_PUB_DEFINE(sensor_cli_pub_1, 20, MSG_ROLE);
+
+static esp_ble_mesh_model_t extend_model_level[] = {
+    ESP_BLE_MESH_MODEL_GEN_LEVEL_CLI(&sensor_cli_pub_1, &sensor_client_1),
+};
+
+static esp_ble_mesh_elem_t elements[] = {
+    ESP_BLE_MESH_ELEMENT(0, root_models, ESP_BLE_MESH_MODEL_NONE),
+    ESP_BLE_MESH_ELEMENT(0, extend_model_level, ESP_BLE_MESH_MODEL_NONE),
+};
+
+static esp_ble_mesh_comp_t composition = {
+    .cid = CID_ESP,
+    .elements = elements,
+    .element_count = ARRAY_SIZE(elements),
+};
+
+static esp_ble_mesh_prov_t provision = {
+    .uuid = dev_uuid
+};
+
 static const char *TAG = "MAIN";
 RTC_NOINIT_ATTR int gateway_mode_flag;
 char version[10] = "2.0.0";
@@ -66,6 +141,211 @@ httpd_handle_t server;
 node_t node[10];
 
 
+static void prov_complete(uint16_t net_idx, uint16_t addr, uint8_t flags, uint32_t iv_index)
+{
+
+    ESP_LOGI(TAG, "net_idx: 0x%04x, addr: 0x%04x", net_idx, addr);
+    ESP_LOGI(TAG, "flags: 0x%02x, iv_index: 0x%08" PRIx32, flags, iv_index);
+
+}
+
+static void example_ble_mesh_provisioning_cb(esp_ble_mesh_prov_cb_event_t event,
+                                             esp_ble_mesh_prov_cb_param_t *param)
+{
+    switch (event) {
+
+    case ESP_BLE_MESH_PROV_REGISTER_COMP_EVT:
+        ESP_LOGI(TAG, "ESP_BLE_MESH_PROV_REGISTER_COMP_EVT, err_code %d", param->prov_register_comp.err_code);
+        // mesh_example_info_restore(); /* Restore proper mesh example info */
+        break;
+    case ESP_BLE_MESH_NODE_PROV_ENABLE_COMP_EVT:
+        ESP_LOGI(TAG, "ESP_BLE_MESH_NODE_PROV_ENABLE_COMP_EVT, err_code %d", param->node_prov_enable_comp.err_code);
+        break;
+    case ESP_BLE_MESH_NODE_PROV_LINK_OPEN_EVT:
+        ESP_LOGI(TAG, "ESP_BLE_MESH_NODE_PROV_LINK_OPEN_EVT, bearer %s",
+            param->node_prov_link_open.bearer == ESP_BLE_MESH_PROV_ADV ? "PB-ADV" : "PB-GATT");
+        break;
+    case ESP_BLE_MESH_NODE_PROV_LINK_CLOSE_EVT:
+        ESP_LOGI(TAG, "ESP_BLE_MESH_NODE_PROV_LINK_CLOSE_EVT, bearer %s",
+            param->node_prov_link_close.bearer == ESP_BLE_MESH_PROV_ADV ? "PB-ADV" : "PB-GATT");
+        break;
+    case ESP_BLE_MESH_NODE_PROV_COMPLETE_EVT:
+        ESP_LOGI(TAG, "ESP_BLE_MESH_NODE_PROV_COMPLETE_EVT");
+        prov_complete(param->node_prov_complete.net_idx, param->node_prov_complete.addr,
+            param->node_prov_complete.flags, param->node_prov_complete.iv_index);
+        ESP_LOGI(TAG, "GATEWAY Provisioned Successfully");
+        break;
+    case ESP_BLE_MESH_NODE_PROV_RESET_EVT:
+        break;
+    case ESP_BLE_MESH_NODE_SET_UNPROV_DEV_NAME_COMP_EVT:
+        ESP_LOGI(TAG, "ESP_BLE_MESH_NODE_SET_UNPROV_DEV_NAME_COMP_EVT, err_code %d", param->node_set_unprov_dev_name_comp.err_code);
+        break;
+    default:
+        break;
+
+    }
+}
+
+// void example_ble_mesh_send_sensor_message(uint32_t opcode)
+// {
+//     esp_ble_mesh_light_client_get_state_t get = {0};
+//     esp_ble_mesh_client_common_param_t common = {0};
+//     esp_ble_mesh_node_t *node = NULL;
+//     esp_err_t err = ESP_OK;
+
+//     example_ble_mesh_set_msg_common(&common, node, sensor_client.model, opcode);
+//     switch (opcode) {
+//     case ESP_BLE_MESH_MODEL_OP_LIGHT_LC_PROPERTY_GET:
+//         get.lc_property_get.property_id = sensor_prop_id;
+//         break;
+//     default:
+//         break;
+//     }
+
+//     err = esp_ble_mesh_sensor_client_get_state(&common, &get);
+//     if (err != ESP_OK) {
+//         ESP_LOGE(TAG, "Failed to send sensor message 0x%04" PRIx32, opcode);
+//     }
+// }
+
+static void example_ble_mesh_config_client_cb(esp_ble_mesh_cfg_server_cb_event_t event,
+                                              esp_ble_mesh_cfg_server_cb_param_t *param)
+{
+    if (event == ESP_BLE_MESH_CFG_SERVER_STATE_CHANGE_EVT) {
+        switch (param->ctx.recv_op) {
+        case ESP_BLE_MESH_MODEL_OP_APP_KEY_ADD:
+            ESP_LOGI(TAG, "ESP_BLE_MESH_MODEL_OP_APP_KEY_ADD");
+            ESP_LOGI(TAG, "net_idx 0x%04x, app_idx 0x%04x",
+                param->value.state_change.appkey_add.net_idx,
+                param->value.state_change.appkey_add.app_idx);
+            ESP_LOG_BUFFER_HEX("AppKey", param->value.state_change.appkey_add.app_key, 16);
+            break;
+        case ESP_BLE_MESH_MODEL_OP_MODEL_APP_BIND:
+            ESP_LOGI(TAG, "ESP_BLE_MESH_MODEL_OP_MODEL_APP_BIND");
+            ESP_LOGI(TAG, "elem_addr 0x%04x, app_idx 0x%04x, cid 0x%04x, mod_id 0x%04x",
+                param->value.state_change.mod_app_bind.element_addr,
+                param->value.state_change.mod_app_bind.app_idx,
+                param->value.state_change.mod_app_bind.company_id,
+                param->value.state_change.mod_app_bind.model_id);
+            break;
+        case ESP_BLE_MESH_MODEL_OP_MODEL_SUB_ADD:
+            ESP_LOGI(TAG, "ESP_BLE_MESH_MODEL_OP_MODEL_SUB_ADD");
+            ESP_LOGI(TAG, "elem_addr 0x%04x, sub_addr 0x%04x, cid 0x%04x, mod_id 0x%04x",
+                param->value.state_change.mod_sub_add.element_addr,
+                param->value.state_change.mod_sub_add.sub_addr,
+                param->value.state_change.mod_sub_add.company_id,
+                param->value.state_change.mod_sub_add.model_id);
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+static void example_ble_mesh_light_client_cb(esp_ble_mesh_light_client_cb_event_t event,
+                                            esp_ble_mesh_light_client_cb_param_t *param)
+{
+    esp_ble_mesh_node_t *node = NULL;
+    ESP_LOGI(TAG, "SENSOR CLIENT, event 0x%04x, addr: %d, RSSI:%d", event, param->params->ctx.addr, param->params->ctx.recv_rssi);
+    if(param->status_cb.ctl_status.present_ctl_lightness)
+    {
+        ESP_LOGI(TAG, "TEMPERATURE NODE: %.2f°C", (float)((param->status_cb.ctl_status.present_ctl_lightness )/100.0) );
+    }
+
+    if(param->error_code)
+    {
+        ESP_LOGE(TAG, "Get lightness client message failed (err %d)", param->error_code);
+        return;
+    }
+
+    // switch(event)
+    // {
+    //     case ESP_BLE_MESH_LIGHT_CLIENT_GET_STATE_EVT:
+    //         ESP_LOGI(TAG, "ESP_BLE_MESH_LIGHT_CLIENT_GET_STATE_EVT");
+    //         if(param->params->opcode == ESP_BLE_MESH_MODEL_OP_GEN_LEVEL_GET)
+    //         {
+    //             ESP_LOGI(TAG, "ESP_BLE_MESH_MODEL_OP_GEN_LEVEL_GET, lightness: %d", param->status_cb.lightness_status.present_lightness);
+    //         }
+    //         break;
+    //     case ESP_BLE_MESH_LIGHT_CLIENT_SET_STATE_EVT:
+    //         ESP_LOGI(TAG,"ESP_BLE_MESH_GENERIC_CLIENT_SET_STATE_EVT" );
+    //         if(param->params->opcode == ESP_BLE_MESH_MODEL_OP_GEN_LEVEL_SET)
+    //         {
+    //             ESP_LOGI(TAG, "ESP_BLE_MESH_MODEL_OP_GEN_LEVEL_SET, lightness: %d", param->status_cb.lightness_status.target_lightness);
+    //         }
+    //         break;
+    //     case ESP_BLE_MESH_LIGHT_CLIENT_PUBLISH_EVT:
+    //         ESP_LOGI(TAG, "ESP_BLE_MESH_NODE_CLIENT_PUBLISH_EVT");
+    //         break;
+    //     case ESP_BLE_MESH_LIGHT_CLIENT_TIMEOUT_EVT:
+    //         ESP_LOGI(TAG, "ESP_BLE_MESH_LIGHT_CLIENT_TIMEOUT_EVT");
+    //         break;
+    //     default:
+    //         break;
+    // }
+}
+
+static void exmaple_ble_mesh_generic_client_cb(esp_ble_mesh_generic_client_cb_event_t event,
+                                               esp_ble_mesh_generic_client_cb_param_t *param)
+{
+    // ESP_LOGI(TAG, "Sensor client, event %u, addr:0x%04x, RSSI:%d", event, param_1->addr, param->params->ctx.recv_rssi);
+    if( param->status_cb.level_status.present_level )
+    {
+        ESP_LOGI(TAG, "Battery Node = %d (mAh)", param->status_cb.level_status.present_level);
+    }
+
+    // switch (event) {
+    // case ESP_BLE_MESH_GENERIC_CLIENT_GET_STATE_EVT:
+    //     ESP_LOGI(TAG, "ESP_BLE_MESH_GENERIC_CLIENT_GET_STATE_EVT");
+    //     if (param->params->opcode == ESP_BLE_MESH_MODEL_OP_GEN_ONOFF_GET) {
+    //         ESP_LOGI(TAG, "ESP_BLE_MESH_MODEL_OP_GEN_ONOFF_GET, onoff %d", param->status_cb.onoff_status.present_onoff);
+    //     }
+    //     break;
+    // case ESP_BLE_MESH_GENERIC_CLIENT_SET_STATE_EVT:
+    //     ESP_LOGI(TAG, "ESP_BLE_MESH_GENERIC_CLIENT_SET_STATE_EVT");
+    //     if (param->params->opcode == ESP_BLE_MESH_MODEL_OP_GEN_ONOFF_SET) {
+    //         ESP_LOGI(TAG, "ESP_BLE_MESH_MODEL_OP_GEN_ONOFF_SET, onoff %d", param->status_cb.onoff_status.present_onoff);
+    //     }
+    //     break;
+    // case ESP_BLE_MESH_GENERIC_CLIENT_PUBLISH_EVT:
+    //     ESP_LOGI(TAG, "ESP_BLE_MESH_GENERIC_CLIENT_PUBLISH_EVT");
+    //     break;
+    // case ESP_BLE_MESH_GENERIC_CLIENT_TIMEOUT_EVT:
+    //     ESP_LOGI(TAG, "ESP_BLE_MESH_GENERIC_CLIENT_TIMEOUT_EVT");
+
+    //     break;
+    // default:
+    //     break;
+    // }
+}
+
+static esp_err_t ble_mesh_init(void)
+{
+    esp_err_t err = ESP_OK;
+
+    esp_ble_mesh_register_prov_callback(example_ble_mesh_provisioning_cb);
+    esp_ble_mesh_register_config_client_callback(example_ble_mesh_config_client_cb);
+    esp_ble_mesh_register_light_client_callback(example_ble_mesh_light_client_cb);
+    esp_ble_mesh_register_generic_client_callback(exmaple_ble_mesh_generic_client_cb);
+
+    err = esp_ble_mesh_init(&provision, &composition);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to initialize mesh stack");
+        return err;
+    }
+
+    err = esp_ble_mesh_node_prov_enable(ESP_BLE_MESH_PROV_ADV | ESP_BLE_MESH_PROV_GATT);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to enable mesh provisioner");
+        return err;
+    }
+
+    ESP_LOGI(TAG, "BLE Mesh Sensor client initialized");
+    return ESP_OK;
+}
+
+
+
 void app_main(void)
 {
     esp_err_t err;
@@ -86,6 +366,19 @@ void app_main(void)
     xTaskCreate(&button_task, "button_task", 2048, NULL, 15, NULL);
     wifi_init();
 
+    err = bluetooth_init();
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "esp32_bluetooth_init failed (err %d)", err);
+        return;
+    }
+
+    ble_mesh_get_dev_uuid(dev_uuid);
+
+    /* Initialize the Bluetooth Mesh Subsystem */
+    err = ble_mesh_init();
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Bluetooth mesh init failed (err %d)", err);
+    }
 
     if (gateway_mode_flag == SMARTCONFIG_MODE)
     {
